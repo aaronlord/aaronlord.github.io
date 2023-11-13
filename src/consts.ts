0694b0619987079e16059081dc18b44b759e5f49
Aaron Lord <hire@aaron.codes>
2023-11-13 15:15:58 +0000

    Projects by date

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Freelance Web Developer / Laravel & Vue / aaron.codes';
-export const SITE_DESCRIPTION = "I'm an experienced full stack developer from the UK, specialising in Laravel and Vue.";
+export const SITE_TITLE = "Hey, I'm Aaron. A freelance web developer specialising in Laravel & Vue.";
+export const SITE_DESCRIPTION = "I'm an experienced full stack developer from the UK, specialising in building Single Page Apps in Laravel and Vue.";
