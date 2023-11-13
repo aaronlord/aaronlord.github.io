feccaa6dff107cb0cf852df0f990a23b15365fc7
Aaron Lord <hire@aaron.codes>
2023-11-13 11:48:44 +0000

    Drafts

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,9 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-    const posts = await getCollection('blog');
+    const posts = await getCollection('blog', ({ data }) => {
+      return import.meta.env.PROD ? data.draft !== true : true;
+    });
 
     return rss({
         title: SITE_TITLE,
