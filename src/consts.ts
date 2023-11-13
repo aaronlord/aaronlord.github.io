0dee8414b2a113f50f7c976c4ec95e541969aaeb
Aaron Lord <hire@aaron.codes>
2023-11-13 16:46:55 +0000

    coding for

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
 export const SITE_TITLE = "Hey, I'm Aaron. A freelance web developer specialising in Laravel & Vue.";
 export const SITE_DESCRIPTION = "I'm an experienced full stack developer from the UK, specialising in building Single Page Apps in Laravel and Vue.";
+export const CODING_SINCE = 2010;
